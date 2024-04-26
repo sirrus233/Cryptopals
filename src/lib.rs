ec6e08b63b482bca5de19d5995672ccd0f2813ea
Bradley Sherman <sirrus233@gmail.com>
2024-04-26 02:04:01 -0700

    Rename single_byte_xor

--- src/lib.rs
+++ src/lib.rs
@@ -49,18 +49,18 @@ fn repeating_key(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
     key.iter().cycle().take(ciphertext.len()).cloned().collect()
 }
 
-pub fn decrypt_single_byte_xor(ciphertext: &[u8], key: u8) -> Vec<u8> {
-    fixed_xor(ciphertext, &repeating_key(ciphertext, &vec![key]))
+pub fn single_byte_xor(data: &[u8], key: u8) -> Vec<u8> {
+    fixed_xor(data, &repeating_key(data, &vec![key]))
 }
 
 pub fn break_single_byte_xor(ciphertext: &[u8]) -> Vec<u8> {
     let (key, _) = (0..u8::MAX)
-        .map(|i| score_english_plaintext(&decrypt_single_byte_xor(ciphertext, i)))
+        .map(|i| score_english_plaintext(&single_byte_xor(ciphertext, i)))
         .enumerate()
         .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
         .unwrap();
 
-    decrypt_single_byte_xor(ciphertext, key as u8)
+    single_byte_xor(ciphertext, key as u8)
 }
 
 pub fn detect_single_byte_xor(ciphertexts: &[&[u8]]) -> Vec<u8> {
